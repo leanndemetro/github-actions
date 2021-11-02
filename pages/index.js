911ba67a7a960f3741fbba569e07b13569bf87ac
Alyssa DeMetro <leanndemetro@gmail.com>
2021-11-02 14:39:45 -0400

    converted to last v

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,8 @@
-import Image from "next/image";
-
-const prefix = "/github-actions";
 
 export default function Home() {
   return (
     <div>
-      <Image src={prefix + "/images/earth.jpg"} height={100} width={100} />
+      &hearts;
     </div>
   )
 }
